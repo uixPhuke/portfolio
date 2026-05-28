0f820cf8217e3711e63ab08bb1401439db29bbe7
uixPhuke <uixphuke@gmail.com>
2026-05-28 05:30:58 +0530

    feat: reorganize Home component structure by updating component order

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,21 @@ import Hero from "@/components/hero"
 import Showcase from "@/components/showcase"
 import Brands from "@/components/brands"
 import Services from "@/components/services"
+import AboutCard from "@/components/about-card"
+import Landing from "@/components/landing"
+import Skills from "@/components/skills"
+import Footer from "@/components/footer"
 
 export default function Home() {
   return (
     <main className="bg-[#0d1117] text-white">
       <Navbar />
-      <Hero />
-      <Showcase />
+      <Landing/>
+     <AboutCard />
       <Brands />
       <Services />
+      <Skills />
+      <Footer/>
     </main>
   )
 }
